c722a5a04770681c65620a57e918450cc1bdcadf
Keith A. Lewis <kal@kalx.net>
2026-05-19 00:06:08 -0400

    partials

--- fms_plackett.h
+++ fms_plackett.h
@@ -12,7 +12,7 @@ namespace fms {
 		template<std::floating_point X>
 		inline X C(X u, X v, X theta)
 		{
-			if (u < 0 or u > 1 or v < 0 or v > 1 or theta  < 0) {
+			if (u < 0 or u > 1 or v < 0 or v > 1 or theta < 0) {
 				return std::numeric_limits<X>::quiet_NaN();
 			}
 
@@ -29,6 +29,76 @@ namespace fms {
 			return (s - std::sqrt(D)) / (2 * theta_1);
 		}
 
+		// Partial derivative of Plackett copula with respect to u
+		// Returns the conditional distribution P(V <= v | U = u)
+		template<std::floating_point X>
+		inline X C_u(X u, X v, X theta)
+		{
+			if (u < 0 or u > 1 or v < 0 or v > 1 or theta < 0) {
+				return std::numeric_limits<X>::quiet_NaN();
+			}
+
+			X theta_1 = theta - 1;
+			X epsilon = std::sqrt(std::numeric_limits<X>::epsilon());
+
+			// Special case: independence
+			if (std::abs(theta_1) < epsilon) {
+				return v;
+			}
+
+			X s = 1 + theta_1 * (u + v);
+			X D = s * s - 4 * theta * theta_1 * u * v;
+			X sqrtD = std::sqrt(D);
+
+			return (sqrtD - s + 2 * theta * theta_1 * v) / (2 * sqrtD);
+		}
+		// Partial derivative of Plackett copula with respect to v
+		// Returns the conditional distribution P(U <= u | V = v)
+		template<std::floating_point X>
+		inline X C_v(X u, X v, X theta)
+		{
+			if (u < 0 or u > 1 or v < 0 or v > 1 or theta < 0) {
+				return std::numeric_limits<X>::quiet_NaN();
+			}
+
+			X theta_1 = theta - 1;
+			X epsilon = std::sqrt(std::numeric_limits<X>::epsilon());
+
+			// Special case: independence
+			if (std::abs(theta_1) < epsilon) {
+				return u;
+			}
+
+			X s = 1 + theta_1 * (u + v);
+			X D = s * s - 4 * theta * theta_1 * u * v;
+			X sqrtD = std::sqrt(D);
+
+			return (sqrtD - s + 2 * theta * theta_1 * u) / (2 * sqrtD);
+		}
+		// Copula density function (second-order mixed partial derivative)
+		template<std::floating_point X>
+		inline X C_uv(X u, X v, X theta)
+		{
+			if (u < 0 or u > 1 or v < 0 or v > 1 or theta < 0) {
+				return std::numeric_limits<X>::quiet_NaN();
+			}
+
+			X theta_1 = theta - 1;
+			X epsilon = std::sqrt(std::numeric_limits<X>::epsilon());
+
+			// Special case: independence
+			if (std::abs(theta_1) < epsilon) {
+				return 1;
+			}
+
+			X s = 1 + theta_1 * (u + v);
+			X D = s * s - 4 * theta * theta_1 * u * v;
+
+			X numerator = theta * (1 + theta_1 * (u + v - 2 * u * v));
+			X denominator = std::pow(D, 1.5);
+
+			return numerator / denominator;
+		}
 
 	} // namespace plackett
 
--- xll_placket.cpp
+++ xll_placket.cpp
@@ -20,3 +20,51 @@ double WINAPI xll_plackett(double u, double v, double theta)
 #pragma XLLEXPORT
 	return plackett::C(u, v, theta);
 }
+
+AddIn xai_plackett_u(
+	Function(XLL_DOUBLE, L"xll_plackett_u", L"PLACKETT.U")
+	.Arguments({
+		Arg(XLL_DOUBLE, L"u", L"first uniform variate."),
+		Arg(XLL_DOUBLE, L"v", L"second uniform variate."),
+		Arg(XLL_DOUBLE, L"theta", L"positive plackett parameter.")
+		})
+	.Category("XLL")
+	.FunctionHelp(L"Derivative with respect to u of Plackett copula")
+);
+double WINAPI xll_plackett_u(double u, double v, double theta)
+{
+#pragma XLLEXPORT
+	return plackett::C_u(u, v, theta);
+}
+
+AddIn xai_plackett_v(
+	Function(XLL_DOUBLE, L"xll_plackett_v", L"PLACKETT.V")
+	.Arguments({
+		Arg(XLL_DOUBLE, L"u", L"first uniform variate."),
+		Arg(XLL_DOUBLE, L"v", L"second uniform variate."),
+		Arg(XLL_DOUBLE, L"theta", L"positive plackett parameter.")
+		})
+	.Category("XLL")
+	.FunctionHelp(L"Derivative with respect to v of Plackett copula")
+);
+double WINAPI xll_plackett_v(double u, double v, double theta)
+{
+#pragma XLLEXPORT
+	return plackett::C_v(u, v, theta);
+}
+
+AddIn xai_plackett_uv(
+	Function(XLL_DOUBLE, L"xll_plackett_uv", L"PLACKETT.UV")
+	.Arguments({
+		Arg(XLL_DOUBLE, L"u", L"first uniform variate."),
+		Arg(XLL_DOUBLE, L"v", L"second uniform variate."),
+		Arg(XLL_DOUBLE, L"theta", L"positive plackett parameter.")
+		})
+	.Category("XLL")
+	.FunctionHelp(L"Density of Plackett copula")
+);
+double WINAPI xll_plackett_uv(double u, double v, double theta)
+{
+#pragma XLLEXPORT
+	return plackett::C_uv(u, v, theta);
+}
